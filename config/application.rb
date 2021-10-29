f6acc469f6811df9e53595a463ac882f100e463d
Anthony Kim <fbssaejun@gmail.com>
2021-10-29 23:35:21 +0000

    add config code to add network permission

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Truckers
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
+    config.web_console.permissions = '10.0.2.2'
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
