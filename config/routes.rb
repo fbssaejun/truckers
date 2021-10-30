ea59a1d72c1df751eda1f1899ad5b7a30d4f2f96
Anthony Kim <fbssaejun@gmail.com>
2021-10-30 23:52:31 +0000

    allow useers to logout

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get 'companies/create'
   get 'users/create'
   post '/login' => 'sessions#new'
-  post '/logout' => 'sessions#destroy', :as => :logout
+  get '/logout' => 'sessions#destroy', :as => :logout
   post '/sessions/create' => 'sessions#create'
 end
