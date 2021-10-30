1d0d91d8ed23f2b252f24558d14555ddf5433b27
Anthony Kim <fbssaejun@gmail.com>
2021-10-30 22:41:35 +0000

    add roleole column to users table, seed user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,13 @@
 
 trucker = Company.create(name: 'Trucker', email: 'inquiries@trucker.com', phone_number: '1-111-222-33333', description: 'Truck rental company')
 
-Truck.create(company_id: 1, name: 'fancy truck', year: 2021, capacity: 1000)
-Truck.create(company_id: 1, name: 'cool truck', year: 2020, capacity: 1300)
-Truck.create(company_id: 1, name: 'classic truck', year: 1999, capacity: 900)
+
+User.create(company_id: 1, name: 'Anthony', role: 'admin', email: 'admin@trucker.com',
+            password_hash: '$2b$12$h0q30TiUGYGhqvnBtm6hdOps3MQhSrOi6C89vyBCcYNqzxkpAoJrm') # 123465
+User.create(company_id: 1, name: 'Franchesca', role: 'driver', email: 'driver@trucker.com',
+            password_hash: '$2b$12$h0q30TiUGYGhqvnBtm6hdOps3MQhSrOi6C89vyBCcYNqzxkpAoJrm') # 123456
+
+
+# Truck.create(company_id: 1, name: 'fancy truck', year: 2021, capacity: 1000, created_at: datetime)
+# Truck.create(company_id: 1, name: 'cool truck', year: 2020, capacity: 1300)
+# Truck.create(company_id: 1, name: 'classic truck', year: 1999, capacity: 900)
